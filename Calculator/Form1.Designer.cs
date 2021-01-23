aded8df2a6720e89f8377bc9fde1f4213f446d87
amsaabesh <mahiraabesh@gmail.com>
2021-01-23 05:54:25 -0800

    Calculator in C#

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -42,53 +42,64 @@ namespace Calculator
             this.button4 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // firstNumberTextBox
             // 
-            this.firstNumberTextBox.Location = new System.Drawing.Point(282, 170);
+            this.firstNumberTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.firstNumberTextBox.ForeColor = System.Drawing.SystemColors.Menu;
+            this.firstNumberTextBox.Location = new System.Drawing.Point(54, 399);
+            this.firstNumberTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.firstNumberTextBox.Name = "firstNumberTextBox";
-            this.firstNumberTextBox.Size = new System.Drawing.Size(214, 20);
+            this.firstNumberTextBox.Size = new System.Drawing.Size(249, 26);
             this.firstNumberTextBox.TabIndex = 0;
             // 
             // resultTextBox
             // 
-            this.resultTextBox.Location = new System.Drawing.Point(282, 285);
+            this.resultTextBox.BackColor = System.Drawing.Color.Red;
+            this.resultTextBox.ForeColor = System.Drawing.SystemColors.Menu;
+            this.resultTextBox.Location = new System.Drawing.Point(356, 399);
+            this.resultTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.resultTextBox.Name = "resultTextBox";
-            this.resultTextBox.Size = new System.Drawing.Size(214, 20);
+            this.resultTextBox.Size = new System.Drawing.Size(249, 26);
             this.resultTextBox.TabIndex = 1;
             // 
             // secondNumberTextBox
             // 
-            this.secondNumberTextBox.Location = new System.Drawing.Point(282, 213);
+            this.secondNumberTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.secondNumberTextBox.ForeColor = System.Drawing.SystemColors.Menu;
+            this.secondNumberTextBox.Location = new System.Drawing.Point(653, 399);
+            this.secondNumberTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.secondNumberTextBox.Name = "secondNumberTextBox";
-            this.secondNumberTextBox.Size = new System.Drawing.Size(214, 20);
+            this.secondNumberTextBox.Size = new System.Drawing.Size(249, 26);
             this.secondNumberTextBox.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(172, 220);
+            this.label2.Location = new System.Drawing.Point(741, 441);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.Size = new System.Drawing.Size(101, 19);
             this.label2.TabIndex = 4;
             this.label2.Text = "Ditiyo Shongkha";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(172, 292);
+            this.label3.Location = new System.Drawing.Point(438, 441);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.Size = new System.Drawing.Size(40, 19);
             this.label3.TabIndex = 5;
             this.label3.Text = "Uttor";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(172, 177);
+            this.label1.Location = new System.Drawing.Point(104, 441);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.Size = new System.Drawing.Size(112, 19);
             this.label1.TabIndex = 6;
             this.label1.Text = "Prothom Shongkha";
             // 
@@ -96,9 +107,10 @@ namespace Calculator
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.Location = new System.Drawing.Point(12, 8);
+            this.button1.Location = new System.Drawing.Point(108, 505);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 119);
+            this.button1.Size = new System.Drawing.Size(122, 125);
             this.button1.TabIndex = 7;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -106,9 +118,10 @@ namespace Calculator
             // button2
             // 
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(12, 319);
+            this.button2.Location = new System.Drawing.Point(309, 505);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(123, 119);
+            this.button2.Size = new System.Drawing.Size(110, 125);
             this.button2.TabIndex = 8;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -116,9 +129,10 @@ namespace Calculator
             // button3
             // 
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(543, 8);
+            this.button3.Location = new System.Drawing.Point(504, 505);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(123, 119);
+            this.button3.Size = new System.Drawing.Size(126, 125);
             this.button3.TabIndex = 9;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -126,9 +140,10 @@ namespace Calculator
             // button4
             // 
             this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(543, 319);
+            this.button4.Location = new System.Drawing.Point(692, 505);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(123, 119);
+            this.button4.Size = new System.Drawing.Size(129, 125);
             this.button4.TabIndex = 10;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
@@ -136,9 +151,10 @@ namespace Calculator
             // button6
             // 
             this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
-            this.button6.Location = new System.Drawing.Point(282, 319);
+            this.button6.Location = new System.Drawing.Point(793, 638);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(123, 119);
+            this.button6.Size = new System.Drawing.Size(128, 127);
             this.button6.TabIndex = 12;
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
@@ -146,18 +162,30 @@ namespace Calculator
             // button5
             // 
             this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
-            this.button5.Location = new System.Drawing.Point(282, 8);
+            this.button5.Location = new System.Drawing.Point(14, 638);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(123, 119);
+            this.button5.Size = new System.Drawing.Size(128, 127);
             this.button5.TabIndex = 13;
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(338, 28);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 249);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(933, 865);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button4);
@@ -170,8 +198,11 @@ namespace Calculator
             this.Controls.Add(this.secondNumberTextBox);
             this.Controls.Add(this.resultTextBox);
             this.Controls.Add(this.firstNumberTextBox);
+            this.Font = new System.Drawing.Font("Mistral", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -191,6 +222,7 @@ namespace Calculator
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
