69b777279faf958f183c24b0e7bf86e5e2c16a82
amsaabesh <mahiraabesh@gmail.com>
2021-01-25 22:52:13 +0600

    Practice

---
+++ Cricket Team/Bangladesh.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cricket_Team
+{
+    public partial class Bangladesh : Form
+    {
+        public Bangladesh()
+        {
+            InitializeComponent();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Akbar Ali is a wicketkeeper Batsman.");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ahmed Mahir Shoaib is the best captain ever.\nHe plays 223 ODIs. 8934 ODI Runs, with 122 avg.\nHe takes 786 odi wickets");
+        }
+    }
+}
---
+++ Cricket Team/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cricket_Team
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //bangladesh
+            Bangladesh ban = new Bangladesh();
+            ban.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //england
+            England eng = new England();
+            eng.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //australia
+            Australia aus = new Australia();
+            aus.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //newzeland
+            Newzeland nz = new Newzeland();
+            nz.ShowDialog();
+        }
+    }
+}
